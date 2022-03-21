a33ee7bb47c3910c82283c130f03f674fad439b6
Vianney Accart <vianneyaccart@gmail.com>
2022-03-21 15:07:48 +0100

    create routing and base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateExerciseComponent } from './pages/create-exercise/create-exercise.component';
+import { ExercisesComponent } from './pages/exercises/exercises.component';
+import { ModifyExerciseComponent } from './pages/modify-exercise/modify-exercise.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/exercices',
+    pathMatch: 'full'
+  },
+  {
+    path: 'exercices',
+    component: ExercisesComponent,
+  },
+  {
+    path: 'exercice/creation',
+    component: CreateExerciseComponent,
+  },
+  {
+    path: 'exercice/:id/modification',
+    component: ModifyExerciseComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExercisesComponent } from './pages/exercises/exercises.component';
+import { CreateExerciseComponent } from './pages/create-exercise/create-exercise.component';
+import { ModifyExerciseComponent } from './pages/modify-exercise/modify-exercise.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ExercisesComponent,
+    CreateExerciseComponent,
+    ModifyExerciseComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
